6030b48ef06123111f1db3196ee4035e871d149c
YvelStLaurent <rachaelbelle@gmail.com>
2019-06-26 17:19:43 -0400

    updating models

--- models/Note.js
+++ models/Note.js
@@ -13,10 +13,6 @@ var NoteSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "Article"
   }
-  // // `title` is of type String
-  // title: String,
-  // // `body` is of type String
-  // body: String
 });
 
 // This creates our model from the above schema, using mongoose's model method
